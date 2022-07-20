bc917a4af5da1a36ffd084ef4b463dada3dc32d0
edoardottt <edoardott@gmail.com>
2022-07-20 11:24:57 +0200

    update dns resolvers

--- cronjob/dns-update.sh
+++ cronjob/dns-update.sh
@@ -2,18 +2,24 @@
 
 path="/home/edoardottt/github/secfiles"
 
-rm -rf "$path/dns/*.txt"
+rm -rf "$path/dns"
+
+mkdir "$path/dns"
 
 date=$(date +%F)
 
 dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 100 -o "$path/dns/resolvers-big.txt"
 
-git add . && git commit -m "$(date) - update dns resolvers big" && git push
+sed -i~ -e '2,$b' -e '/^$/d' "$path/dns/resolvers-big.txt"
+
+rm -rf "/home/edoardottt/github/secfiles/dns/resolvers-big.txt~"
+
+# git add . && git commit -m "$date - update dns resolvers big" && git push
 
 cat "$path/dns/resolvers-big.txt" | head -n 400 > "$path/dns/resolvers-medium.txt"
 
-git add . && git commit -m "$(date) - update dns resolvers medium" && git push
+# git add . && git commit -m "$date - update dns resolvers medium" && git push
 
 cat "$path/dns/resolvers-big.txt" | head -n 30 > "$path/dns/resolvers-small.txt"
 
-git add . && git commit -m "$(date) - update dns resolvers small" && git push
+# git add . && git commit -m "$date - update dns resolvers small" && git push
